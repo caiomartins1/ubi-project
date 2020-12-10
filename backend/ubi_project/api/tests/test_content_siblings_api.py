db3d2122e97d8a4f812aec9bf65ec2f5825bf606
Caio Martins <dev.caiomartins@gmail.com>
2020-12-10 18:10:54 +0000

    Added content sibling detail test, fixed linting issues

--- backend/ubi_project/api/tests/test_content_siblings_api.py
+++ backend/ubi_project/api/tests/test_content_siblings_api.py
@@ -13,6 +13,14 @@ from core.tests.test_models import sample_client, sample_content
 CONTENT_SIBLINGS_URL = reverse('api:contentsibling-list')
 
 
+def detail_url(content_sibling_uuid):
+    """Return ContentSibling detail url"""
+    return reverse(
+        'api:contentsibling-detail',
+        args=[content_sibling_uuid],
+    )
+
+
 class ContentSiblingTest(TestCase):
     """Test the ContentSibling API"""
 
@@ -41,11 +49,31 @@ class ContentSiblingTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
+    def test_view_content_sibling_detail(self):
+        """Test viewing a content sibling detail"""
+        client = sample_client()
+        parent = sample_content(client)
+        content = sample_content(client, 'Example', 'Example description')
+
+        content_sibling = ContentSibling.objects.create(
+            parent=parent,
+            content=content,
+        )
+
+        url = detail_url(content_sibling.uuid)
+        res = self.client.get(url)
+        serializer = ContentSiblingSerializer(content_sibling)
+        self.assertEqual(res.data, serializer.data)
+
     def test_create_content_sibling(self):
         """Test creating a content sibling"""
         client = sample_client()
         parent = sample_content(client)
-        content = sample_content(client, 'Example location', 'Example description')
+        content = sample_content(
+            client,
+            'Example location',
+            'Example description'
+        )
         payload = {
             'parent': parent.uuid,
             'content': content.uuid
